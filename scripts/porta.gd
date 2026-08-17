ab98db0f59936f792fafc1d8a1a22f5dac11b50e
Gabriel Milanez <ghmilanez@gmail.com>
2026-08-17 20:15:52 -0300

    changes

--- scripts/porta.gd
+++ scripts/porta.gd
@@ -3,3 +3,7 @@ extends StaticBody2D
 func abrir(_body: Node2D = null) -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
 	visible = false
+
+
+func _on_flag_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
